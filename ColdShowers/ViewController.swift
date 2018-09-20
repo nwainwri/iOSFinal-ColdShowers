aeca2f620491c9d00ef2bc8d6118e991017d4938
HighGroundLover <kclarkoneil@yahoo.ca>
2018-09-20 14:13:12 -0700

    Initial Commit

---
+++ ColdShowers/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ColdShowers
+//
+//  Created by Kit Clark-O'Neil on 2018-09-20.
+//  Copyright © 2018 Kit Clark-O'Neil. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
