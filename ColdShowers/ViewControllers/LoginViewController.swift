796a50d0dd26f0916f3055367ab50b2b820dd620
nwainwri <nathanwainwright@gmail.com>
2018-09-21 17:13:17 -0700

    ending Testing for Now

--- ColdShowers/ViewControllers/LoginViewController.swift
+++ ColdShowers/ViewControllers/LoginViewController.swift
@@ -8,6 +8,9 @@
 
 import UIKit
 import UserNotifications
+
+import CoreData
+
 class LoginViewController: UIViewController {
   
   // MARK: LoginView Properties
@@ -19,7 +22,38 @@ class LoginViewController: UIViewController {
   
     override func viewDidLoad() {
         super.viewDidLoad()
-
+      
+      
+//      //MARK: Testing Core Data
+//      let appDelegate = UIApplication.shared.delegate as! AppDelegate
+//      let context = appDelegate.persistentContainer.viewContext
+//
+//      let testStrength = NSEntityDescription.insertNewObject(forEntityName: "Activities", into: context) as! Activities
+//
+//      testStrength.id = 0
+//      testStrength.name = "Push Ups"
+//      testStrength.areaBody = ["arms", "chest"]
+//      testStrength.style = "strength"
+//      testStrength.instructPhoto = ""
+//
+//      let testYoga = NSEntityDescription.insertNewObject(forEntityName: "Activities", into: context) as! Activities
+//
+//      testYoga.id = 0
+//      testYoga.name = "Mountain Pose"
+//      testYoga.areaBody = ["legs", "arms"]
+//      testYoga.style = "yoga"
+//      testYoga.instructPhoto = ""
+//
+//      let testMindful = NSEntityDescription.insertNewObject(forEntityName: "Activities", into: context) as! Activities
+//
+//      testMindful.id = 0
+//      testMindful.name = "Be Still"
+//      testMindful.areaBody = ["brain", "soul"]
+//      testMindful.style = "mindful"
+//      testMindful.instructPhoto = ""
+//
+//      appDelegate.saveContext()
+      
         // Do any additional setup after loading the view.
     }
 
