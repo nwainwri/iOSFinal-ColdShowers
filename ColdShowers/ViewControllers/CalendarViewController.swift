c2c3bec0b1e990f64d55905e802c297c033cc40a
HighGroundLover <kclarkoneil@yahoo.ca>
2018-09-24 11:06:52 -0700

    completionHandler, setactivity function completed but untested

--- ColdShowers/ViewControllers/CalendarViewController.swift
+++ ColdShowers/ViewControllers/CalendarViewController.swift
@@ -23,6 +23,7 @@ class CalendarViewController: UIViewController {
 
   @IBOutlet weak var calenderViewDoneButton: UIButton!
 
+//    var hasPermission:Bool
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -53,39 +54,56 @@ class CalendarViewController: UIViewController {
         
         
     }
-    func checkUserPermission () {
+    func checkUserPermission(request: @escaping (_ request:Bool) -> Bool) {
         UNUserNotificationCenter.current().getNotificationSettings { (notificationSettings) in
             switch notificationSettings.authorizationStatus {
             case .notDetermined:
                 self.requestUserPermission(completionHandler: { _ in
-                        self.checkUserPermission()
-    
+                    self.checkUserPermission(request: request)
                 })
                 
             case .authorized:
-                
+            _ = request(true)
                 
             case .denied:
+            _ = request(false)
+            
+            let alert = UIAlertController(title: "Permission Denied",
+                                          message: "Notification access denied, would you like to enable?",
+                                          preferredStyle: UIAlertControllerStyle.alert);
+            self.present(alert, animated: true, completion: nil)
                 
             }
         }
-    }
-//  func setActivity(date: Date, repeats: Bool) {
-//    
-//    
-//    
-//    
-//    var myDateComponents = DateComponents()
-//    let calendar = Calendar.current
-//    
-//    myDateComponents.hour = calendar.component(.hour, from: date)
-//    myDateComponents.day = calendar.component(.day, from: date)
-//    myDateComponents.minute = calendar.component(.minute, from: date)
-//    
-//    let trigger = UNCalendarNotificationTrigger(dateMatching: myDateComponents, repeats: repeats)
-//    let request = UNNotificationRequest(identifier: "uniqueID", content: <#T##UNNotificationContent#>, trigger: <#T##UNNotificationTrigger?#>)
-//
-//  }
-//  
 
+        func setActivity(dates: [Date], repeats: Bool) {
+            
+            checkUserPermission { (res) -> Bool in
+                if res {
+                    for date in dates {
+                    let notificationContent = UNMutableNotificationContent()
+                    notificationContent.title = "Wake up"
+                    notificationContent.body = "Jump in?"
+                    
+                    var myDateComponents = DateComponents()
+                    let calendar = Calendar.current
+                    
+                    myDateComponents.hour = calendar.component(.hour, from: date)
+                    myDateComponents.day = calendar.component(.day, from: date)
+                    myDateComponents.minute = calendar.component(.minute, from: date)
+                    
+                    let trigger = UNCalendarNotificationTrigger(dateMatching: myDateComponents, repeats: repeats)
+                    let request = UNNotificationRequest(identifier: "uniqueID", content: notificationContent, trigger: trigger)
+                    UNUserNotificationCenter.current().add(request, withCompletionHandler: { (error) in
+                        if let error = error {
+                            print(error)
+                        }
+                    })
+                    }
+                }
+                else {
+                    return res
+                }
+            }
+        }
 }
