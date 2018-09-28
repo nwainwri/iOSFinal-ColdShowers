ee34135901e7555044769e5f2d93711990dd4dac
HighGroundLover <kclarkoneil@yahoo.ca>
2018-09-27 17:59:35 -0700

    ScheduleView first commit

---
+++ ColdShowers/ViewControllers/ScheduleViewController.swift
@@ -0,0 +1,98 @@
+//
+//  ScheduleViewController.swift
+//  ColdShowers
+//
+//  Created by Kit Clark-O'Neil on 2018-09-27.
+//  Copyright © 2018 Kit Clark-O'Neil. All rights reserved.
+//
+
+import UIKit
+import UserNotifications
+class ScheduleViewController: UITableViewController {
+
+    var myAlarms = [UNNotificationRequest]()
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        UNUserNotificationCenter.current().getPendingNotificationRequests { (myRequests) in
+            self.myAlarms = myRequests
+        }
+    }
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 0
+    }
+
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "AlarmCell", for: indexPath) as? AlarmCell else { fatalError() }
+        
+        let alarm = myAlarms[indexPath.row]
+        cell.dateLabel.text = "\(alarm.description)"
+        cell.timeLabel.text = "time"
+        
+        
+
+        return cell
+    }
+ 
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
