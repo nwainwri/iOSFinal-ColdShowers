174d6e4677a3157b8fad6aca8a74698429ea8a2e
nwainwri <nathanwainwright@gmail.com>
2018-10-01 12:36:29 -0700

    code cleaned up

--- ColdShowers/Models/DefaultSet.swift
+++ ColdShowers/Models/DefaultSet.swift
@@ -9,64 +9,8 @@
 import UIKit
 import CoreData
 
-
-// strengthOne, strengthTwo, strengthThree, strengthFour, strengthFive, strengthSix, strengthSeven, strengthEight, strengthNine,
-let strengthZero = Activity(name: "Pushups", areaOfBody: ["chest", "arms"], instructions: "pushups", category: .strength, photo: "pushups")
-let strengthOne = Activity(name: "Sit Ups", areaOfBody: ["stomach", "legs"], instructions: "", category: .strength, photo: "situps")
-let strengthTwo = Activity(name: "Crunches", areaOfBody: ["stomach"], instructions: "", category: .strength, photo: "crunches")
-let strengthThree = Activity(name: "Leg Lifts ", areaOfBody: ["legs"], instructions: "", category: .strength, photo: "leglifts")
-let strengthFour = Activity(name: "Squats", areaOfBody: ["legs","back"], instructions: "", category: .strength, photo: "squats")
-let strengthFive = Activity(name: "Plank", areaOfBody: ["back","stomach", "arms"], instructions: "", category: .strength, photo: "plank")
-let strengthSix = Activity(name: "Lunges", areaOfBody: ["legs"], instructions: "", category: .strength, photo: "lunges")
-let strengthSeven = Activity(name: "Pike Pushup", areaOfBody: ["back","legs", "arms"], instructions: "", category: .strength, photo: "pikePushUp")
-let strengthEight = Activity(name: "Bear Crawl", areaOfBody: ["back","arms"], instructions: "", category: .strength, photo: "bearcrawl")
-let strengthNine = Activity(name: "Hip/Thigh Extension", areaOfBody: ["legs","stomach"], instructions: "", category: .strength, photo:"hipthigh")
-
-// mindZero, mindTwo, mindThree, mindFour, mindFive, mindSix, mindSeven, mindEight, mindNine,
-let mindZero = Activity(name: "Focus on a Single Minute", areaOfBody: ["mind","soul"], instructions: "", category: .mindfull, photo:"mindful")
-let mindOne = Activity(name: "Focus on a Single Object", areaOfBody: ["mind","soul"], instructions: "", category: .mindfull, photo:"mindful")
-let mindTwo = Activity(name: "Band of Light", areaOfBody: ["mind","soul"], instructions: "", category: .mindfull, photo:"mindful")
-let mindThree = Activity(name: "Inner-Outer Experience", areaOfBody: ["mind","soul"], instructions: "", category: .mindfull, photo:"mindful")
-let mindFour = Activity(name: "Record Three Minutes of Thoughts", areaOfBody: ["mind","soul"], instructions: "", category: .mindfull, photo:"mindful")
-let mindFive = Activity(name: "Thought Defusion", areaOfBody: ["mind","soul"], instructions: "", category: .mindfull, photo:"mindful")
-let mindSix = Activity(name: "Describe Your Emotion", areaOfBody: ["mind","soul"], instructions: "", category: .mindfull, photo:"mindful")
-let mindSeven = Activity(name: "Focus Shifting", areaOfBody: ["mind","soul"], instructions: "", category: .mindfull, photo:"mindful")
-let mindEight = Activity(name: "Mindful Breathing", areaOfBody: ["mind","soul"], instructions: "", category: .mindfull, photo:"mindful")
-let mindNine = Activity(name: "Mindful Seeing", areaOfBody: ["mind","soul"], instructions: "", category: .mindfull, photo:"mindful")
-
-// yogaZero, yogaTwo, yogaThree, yogaFour, yogaFive, yogaSix, yogaSeven, yogaEight, yogaNine,
-let yogaZero = Activity(name: "Mountain Pose", areaOfBody: ["legs", "arms"], instructions: "", category: .yoga , photo:"mountain")
-let yogaOne = Activity(name: "Tree Pose", areaOfBody: ["legs"], instructions: "", category: .yoga, photo:"tree")
-let yogaTwo = Activity(name: "Child's Pose", areaOfBody: ["legs","arms"], instructions: "", category: .yoga, photo:"child")
-let yogaThree = Activity(name: "Downward Facing Dog", areaOfBody: ["legs","back", "arms"], instructions: "", category: .yoga, photo:"downwarddog")
-let yogaFour = Activity(name: "Warrior I", areaOfBody: ["legs","arms"], instructions: "", category: .yoga, photo:"warriorOne")
-let yogaFive = Activity(name: "Triangle Pose", areaOfBody: ["legs","back"], instructions: "", category: .yoga, photo:"triangle")
-let yogaSix = Activity(name: "Chair Pose", areaOfBody: ["legs","back", "arms"], instructions: "", category: .yoga, photo:"chair")
-let yogaSeven = Activity(name: "Cobra Pose", areaOfBody: ["back","arms"], instructions: "", category: .yoga, photo:"cobra")
-let yogaEight = Activity(name: "Cobbler’s Pose", areaOfBody: ["legs"], instructions: "", category: .yoga, photo:"cobbler")
-let yogaNine = Activity(name: "Corpose Pose", areaOfBody: ["body","mind"], instructions: "", category: .yoga, photo:"corpse")
-
 class DefaultSet {
-//  let activities: [Activity]
-//  let categories: [category]
-//  enum category:String {
-//    case strength = "Strength"
-//    case mindfull = "Mindful"
-//    case yoga = "Yoga"
-////    case noOption = "No Option"
-////    func getTitle() -> String {
-////      switch self {
-////      case .strength:
-////        return "Strength"
-////      case .mindfull:
-////        return "Mindful"
-////      case .yoga:
-////        return "Yoga"
-////      case .noOption:
-////        return "NO TITLE"
-////      }
-////    }
-//  }
+  
   init() {
     
     guard let appDelegate =
@@ -79,12 +23,6 @@ class DefaultSet {
     //MARK: sets up initial settings for workout times; only does this once on intial loading of local data
     _ = ActivityTimes(context: context, timeStrengthValue: 1.0, timeMindfulValue: 1.0, timeYogaValue: 1.0)
     
-    
-    
-    
-    
-    
-
     // strength category is Int == 0
     _ = CoreActivity(context:context, name: "Pushups", areaOfBody: ["chest", "arms"], instructions: "", category: 0, photo: "pushups")
     _ = CoreActivity(context: context, name: "Sit Ups", areaOfBody: ["stomach", "legs"], instructions: "", category: 0, photo: "situps")
@@ -98,7 +36,6 @@ class DefaultSet {
     _ = CoreActivity(context: context, name: "Hip/Thigh Extension", areaOfBody: ["legs","stomach"], instructions: "", category: 0, photo:"hipthigh")
     
     // mindful category is Int == 1
-    
     _ = CoreActivity(context: context,
                      name: "Focus on a Single Minute",
                      areaOfBody: ["mind","soul"],
@@ -169,7 +106,6 @@ class DefaultSet {
                      category: 1,
                      photo:"mindful")
     
-    
     // yoga category is Int == 2
     _ = CoreActivity(context: context, name: "Mountain Pose", areaOfBody: ["legs", "arms"], instructions: "", category: 2, photo:"mountain")
     _ = CoreActivity(context: context, name: "Tree Pose", areaOfBody: ["legs"], instructions: "", category: 2, photo:"tree")
@@ -181,45 +117,10 @@ class DefaultSet {
     _ = CoreActivity(context: context, name: "Cobra Pose", areaOfBody: ["back","arms"], instructions: "", category: 2, photo:"cobra")
     _ = CoreActivity(context: context, name: "Cobbler’s Pose", areaOfBody: ["legs"], instructions: "", category: 2, photo:"cobbler")
     _ = CoreActivity(context: context, name: "Corpose Pose", areaOfBody: ["body","mind"], instructions: "", category: 2, photo:"corpse")
-
+    
     appDelegate.saveContext()
   }
-  
-//  override init() {
-//    self.activities = [strengthZero, strengthOne, strengthTwo, strengthThree, strengthFour, strengthFive, strengthSix, strengthSeven, strengthEight, strengthNine, mindZero, mindOne, mindTwo, mindThree, mindFour, mindFive, mindSix, mindSeven, mindEight, mindNine, yogaZero, yogaOne, yogaTwo, yogaThree, yogaFour, yogaFive, yogaSix, yogaSeven, yogaEight, yogaNine]
-//    for item in self.activities {
-//      var counter = 0
-//      item.originalIndex = self.activities.index(of: item)!
-//      counter += 1
-//    }
-//    self.categories = [.strength, .mindfull, .yoga]
-//  }
 }
-
-//extension DefaultSet {
-//  func retAmounts(category: category) -> Int {
-//    var innerCount = 1
-//    for item in self.activities {
-//      if item.category.rawValue == category.hashValue {
-//        innerCount += 1
-//      }
-//    }
-//    return innerCount
-//  }
-//
-//  func retAmountsInt(category: Int) -> Int {
-//    var innerCount = 1
-//    for item in self.activities {
-//      if item.category.rawValue == category {
-//        innerCount += 1
-//      }
-//    }
-//    return innerCount
-//  }
-//
-//
-//
-//}
 
 extension CoreActivity {
   convenience init(context: NSManagedObjectContext, name: String, areaOfBody: [String], instructions: String, category: Int, photo: String) {
